a4cf560ce0d8e256a4b3cc15f7dc90f9dba1bd8e
heatroom <2257505815@qq.com>
2014-09-07 10:49:46 +0800

    增加useCapture参数

--- src/event/Event.js
+++ src/event/Event.js
@@ -1,65 +1,65 @@
 /**
  * 事件对象
- * 
+ *
  * @ignore
  * @author Ricky
  */
 define(function (require) {
     var _ = require('underscore').noConflict();
-    
+
     /**
      * 事件对象
-     * 
+     *
      * @constructor
      * @param {HTMLEvent} domEvent DOM事件对象
      */
     function Event(domEvent) {
         domEvent = domEvent || window.event;
-        
+
         //复制属性
         _.each(domEvent, function(value, key) {
             if (!_.isFunction(value)) {
                 this[key] = value;
             }
         }, this);
-        
+
         /**
          * @property {HTMLEvent} originalEvent DOM事件对象
          * @readonly
          */
         this.originalEvent = domEvent;
-        
+
         /**
          * @property {String} type 事件类型
          * @readonly
          */
         this.type = domEvent.type;
-        
+
         /**
          * @property {HTMLElement} target 当前事件的目标对象
          * @readonly
          */
         var target = domEvent.target || domEvent.srcElement;
         this.target = target.nodeType === 3 ? target.parentNode : target;
-        
+
         /**
          * @property {HTMLElement} relatedTarget target关联结点
          * @readonly
          */
         this.relatedTarget = domEvent.relatedTarget || domEvent[(this.type === 'mouseover' ? 'from' : 'to') + 'Element'];
-        
+
         /**
          * @property {Number} keyCode 键码
          * @readonly
          */
         this.keyCode = domEvent.keyCode || domEvent.which;
-        
+
         /**
          * @property {Boolean} rightClick 鼠标右键是否按下
          * @readonly
          */
         this.rightClick = domEvent.which === 3 || domEvent.button === 2;
-        
+
         /**
          * @property {Number} pageX 文档X坐标
          * @readonly
@@ -73,10 +73,10 @@ define(function (require) {
         this.pageX = domEvent.pageX || domEvent.clientX + (root && root.scrollLeft || 0) - (root && root.clientLeft || 0);
         this.pageY = domEvent.pageY || domEvent.clientY + (root && root.scrollTop || 0) - (root && root.clientTop || 0);
     }
-    
+
     Event.prototype = {
         /**
-         * 阻止默认操作 
+         * 阻止默认操作
          */
         preventDefault: function() {
             var e = this.originalEvent;
@@ -86,9 +86,9 @@ define(function (require) {
                 e.returnValue = false;
             }
         },
-        
+
         /**
-         * 阻止冒泡 
+         * 阻止冒泡
          */
         stopPropagation: function() {
             var e = this.originalEvent;
@@ -99,38 +99,39 @@ define(function (require) {
             }
         }
     };
-    
+
     /**
      * 添加事件
-     * 
+     * https://developer.mozilla.org/en-US/docs/Web/API/EventTarget.addEventListener
      * @param {HTMLElement} element DOM元素
      * @param {String} type 事件类型
      * @param {Function} listener 事件处理函数
+     * @param {Boolean} capture 捕获模式
      * @static
      */
-    Event.on = function (element, type, listener) {
+    Event.on = function (element, type, listener, capture) {
         if (element.addEventListener) {
-            element.addEventListener(type, listener, false);
+            element.addEventListener(type, listener, capture || false);
         } else if (element.attachEvent) {
             element.attachEvent('on' + type, listener);
         }
     };
-    
+
     /**
      * 移除事件
-     * 
      * @param {HTMLElement} element DOM元素
      * @param {String} type 事件类型
      * @param {Function} listener 事件处理函数
+     * @param {Boolean} capture 捕获模式
      * @static
      */
-    Event.off = function (element, type, listener) {
+    Event.off = function (element, type, listener, capture) {
         if (element.addEventListener) {
-            element.removeEventListener(type, listener, false);
+            element.removeEventListener(type, listener, capture || false);
         } else if (element.attachEvent) {
             element.detachEvent('on' + type, listener);
         }
     };
-    
+
     return Event;
 });
