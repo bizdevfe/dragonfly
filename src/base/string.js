e08aa73195bb20c07f91b6ae0c89a7f9b3c712e3
heatroom <2257505815@qq.com>
2014-09-08 09:15:36 +0800

    增加string.trim方法

--- src/base/string.js
+++ src/base/string.js
@@ -1,25 +1,50 @@
 /**
  * 字符串扩展
- * 
+ *
  * @ignore
  */
 define(function (require) {
     /**
      * 字符串扩展
-     * 
+     *
      * @class string
      * @extends base
      * @singleton
      */
     var string = {};
-    
+
     /**
      * 字符串两端去空
-     * 
+     *
      */
-    string.trim = function() {
-        
+    string.trim = function(str) {
+        if (str.trim) {
+            return str.trim();
+        }
+        return str.replace(/^\s*|\s*$/, '');
     };
-    
+
+    /*
+     * 字符串左端去空
+     *
+     */
+    string.trimLeft = function(str) {
+        if (str.trimLeft) {
+            return str.trimLeft();
+        }
+        return str.replace(/^\s*/, '');
+    };
+
+    /**
+     * 字符串右端去空
+     *
+     */
+    string.trimRight = function(str) {
+        if (str.trimRight) {
+            return str.trimRight();
+        }
+        return str.replace(/\s*$/, '');
+    };
+
     return string;
 });
